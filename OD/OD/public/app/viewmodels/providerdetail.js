047bc2f84e25cd3fe8a6bb87ae067fedfe4cb8c1
roysbailey <roysbailey@silvertouch.co.uk>
2013-12-18 21:29:00 +0000

    Working on isDirty working on successive screen visits

--- OD/OD/public/app/viewmodels/providerdetail.js
+++ OD/OD/public/app/viewmodels/providerdetail.js
@@ -1,51 +1,47 @@
 ﻿define(
     ['plugins/router','durandal/system', 'services/logger', 'knockout', 'knockout.mapping', 'services/kodirty', 'services/datacontext'],
     function(router, system, logger, ko, komap, kodirty, datacontext) {
-    
-        var provider = ko.observable();
 
-        var dirtyFlag = ko.observable(false);
+        var vm = function() {
 
-        var activate = function (routeData) {
-            log('[providerdetail] view activated', routeData, true);
+            var that = this;
 
-            var prov = datacontext.getProviderByUkprn(routeData);
-            if (prov) {
-                // Note. as we have "required" ko mapping as a module, it is NOT put into the "ko" object as "ko.mapping"
-                // Instead, it is put into our "required" variable.
-                // so, instead of this... provider = ko.mapping.formJS(prov);  you do this...
-                var obj = komap.fromJS(prov);
-                provider(obj);
-                dirtyFlag = kodirty.dirtyFlag(provider);
-                log('Provider loaded and [observable]', provider().ukprn(), true);
+            that.provider = ko.observable();
+        
+            that.dirtyFlag = ko.observable(false);
+
+            that.activate = function (routeData) {
+                log('[providerdetail] view activated', routeData, true);
+
+                var prov = datacontext.getProviderByUkprn(routeData);
+                if (prov) {
+                    // Note. as we have "required" ko mapping as a module, it is NOT put into the "ko" object as "ko.mapping"
+                    // Instead, it is put into our "required" variable.
+                    // so, instead of that... provider = ko.mapping.formJS(prov);  you do that...
+                    var obj = komap.fromJS(prov);
+                    that.provider(obj);
+                    that.dirtyFlag = new kodirty.dirtyFlag(that.provider);
+                    log('Provider loaded and [observable]', that.provider().ukprn(), true);
+                } else {
+                    that.dirtyFlag = ko.observable(false);
+                }
+            };
+
+            that.save = function() {
+                // Convert our observable into a POJO and save
+                var rawProvider = komap.toJS(that.provider);
+                datacontext.saveProvider(rawProvider);
+                router.navigate('#/');
             }
-        };
-
-        var save = function() {
-            // Convert our observable into a POJO and save
-            var rawProvider = komap.toJS(provider);
-            datacontext.saveProvider(rawProvider);
-            router.navigate('#/');
         }
 
-        var vm = 
-        {
-            activate: activate,
-            provider: provider,
-            save: save,
-            dirtyFlag: dirtyFlag
-        };
-
-        return vm;
+        return new vm();
 
         //#region Internal Methods
 
         function log(msg, data, showToast) {
             logger.log(msg, data, system.getModuleId(vm), showToast);
         }
-
-
-
 
         //#endregion
 
